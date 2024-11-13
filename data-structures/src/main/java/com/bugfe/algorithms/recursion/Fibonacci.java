2c6bcceca8216546cd6f7c6fe0569a40a4024bb1
bwf <2304576239@qq.com>
2024-11-13 10:35:48 +0800

    Fibonacci  斐波那契

---
+++ data-structures/src/main/java/com/bugfe/algorithms/recursion/Fibonacci.java
@@ -0,0 +1,31 @@
+package com.bugfe.algorithms.recursion;
+
+/**
+ * 斐波那契数列： 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377......
+ * n=0 fib(0),
+ * n=1 或者 n=2  fib(1),
+ * n > 2   fib(n-1)+fib(n-2)
+ */
+public class Fibonacci {
+
+    public static int fib(int n) {
+        if (n < 0) {
+            throw new RuntimeException("必须大于0");
+        }
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(8));
+    }
+
+
+}
